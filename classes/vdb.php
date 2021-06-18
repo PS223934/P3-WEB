ad86573ca5cbfcd0205800079c4223ef51d5e34f
stbrouwers <33906225+PS223934@users.noreply.github.com>
2021-06-18 12:24:55 +0200

    Create vdb.php

---
+++ classes/vdb.php
@@ -0,0 +1,20 @@
+<?php
+    class vdb 
+    {
+        const DSN = "mysql:host=localhost;dbname=verkiezingenprj3";
+        const USER = "root";
+        const PASSWD = "";
+
+        function GetPartijen(){		
+            $pdo = new PDO(self::DSN, self::USER, self::PASSWD);
+
+            $statement = $pdo->prepare("SELECT * FROM partij;");  
+            
+            $statement->execute(); 
+            
+            $rows = $statement->fetchAll(PDO::FETCH_ASSOC);
+            
+            return $rows;
+        }
+    }
+?>
