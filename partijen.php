d7b68d1cf722e05b92b136bde92709af217f4197
stbrouwers <33906225+PS223934@users.noreply.github.com>
2021-06-18 12:23:39 +0200

    Update and rename partijen.html to partijen.php

---
+++ partijen.php
@@ -0,0 +1,46 @@
+<?php
+    include_once "./classes/vdb.php";
+    $db = new vdb();
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>verkiezingen</title>
+        <link rel="stylesheet" href="style/style.css">
+        <script src="js/vk.js"></script>
+    </head>
+    <body>
+        <ul id='navul'>
+            <li onclick="redirect('index.php')" class="navitem" id="homebtn"><p>⌂</p></li>
+            <li class="navitem hoverdiv navselected">
+             
+                <a href="partijen.php"><p id="spnav">partijen</p></a>
+            
+                <div class="hoverbox"> 
+                    <?php
+
+                    $rows = $db->GetPartijen();
+
+                    foreach($rows as $row) {
+
+                        echo "
+                            <div onclick=redirect('Partijen.php?id=$row[PartijId]') class='hoverbox-content'>
+                                <p>$row[PartijName]</p>
+                            </div>
+                            ";
+                    }
+                    ?>
+                </div>
+            </li>
+            <li onclick="redirect('verkiezingen.php')" class="navitem"><p>verkiezingen</p></li>
+            <li onclick="redirect('themas.php')" class="navitem"><p>thema's</p></li>
+        </ul>
+
+        <div id="content">
+            <div id="contentcontainer">
+
+            </div>
+        </div>
+    </body>
+</html>
