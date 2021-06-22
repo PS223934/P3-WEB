fb5f8a10752d2ed1199935f81497ef542ec3678a
stbrouwers <33906225+PS223934@users.noreply.github.com>
2021-06-22 15:31:34 +0200

    Update partijen.php

--- partijen.php
+++ partijen.php
@@ -19,27 +19,64 @@
             
                 <div class="hoverbox"> 
                     <?php
+                        $rows = $db->GetPartijen();
 
-                    $rows = $db->GetPartijen();
+                            foreach($rows as $row) {
 
-                    foreach($rows as $row) {
+                            echo "
+                                <div onclick=redirect('Partijen.php?id=$row[PartijId]') class='hoverbox-content'>
+                                    <p>$row[PartijName]</p>
+                                </div>
+                                ";
+                        }
+                        ?>
+                    </div>
+                </li>
+                <li onclick="redirect('verkiezingen.php')" class="navitem"><p>verkiezingen</p></li>
+                <li onclick="redirect('themas.php')" class="navitem"><p>thema's</p></li>
+            </ul>
 
+            <div id="content">
+                <div id="contentcontainer">
+                    <?php
+
+                    if (empty($_GET)) {
                         echo "
-                            <div onclick=redirect('Partijen.php?id=$row[PartijId]') class='hoverbox-content'>
-                                <p>$row[PartijName]</p>
-                            </div>
+                            <h1>Partijen</h1>
+                        ";
+
+                        $content = $db->GetPartijen();
+
+                        foreach($content as $row) {
+
+                            echo "
+                                <div class='partijcontainer' onclick=redirect('partijen.php?id=$row[PartijId]')>
+                                    <h1>$row[PartijName]</h1>
+                                </div>
                             ";
+
+                        }
                     }
-                    ?>
-                </div>
-            </li>
-            <li onclick="redirect('verkiezingen.php')" class="navitem"><p>verkiezingen</p></li>
-            <li onclick="redirect('themas.php')" class="navitem"><p>thema's</p></li>
-        </ul>
+                    else {
+                        $pid = $_GET['id'];
+                        $content = $db->GetPartijDetails($pid);
 
-        <div id="content">
-            <div id="contentcontainer">
+                        foreach($content as $row) {
 
+                            echo "
+                                <h1>$row[PartijName]</h1>
+                                <h2>Herkomst:</h2>
+                                <p><b>Gemeente:</b>  $row[Gemeente]<p>
+                                <p><b>Adres:</b>  $row[Adres]<p>
+                                <p><b>Postcode:</b>  $row[Postcode]<p>
+                                <br>
+                                <h2>Contact informatie:</h2>
+                                <p><b>Email Adres:</b>  $row[EmailAdres]<p>
+                                <p><b>Telefoonnummer:</b>  $row[Telefoonnummer]<p>
+                            ";
+                        }
+                    }
+                ?>
             </div>
         </div>
     </body>
